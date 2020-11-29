98f5beea3dae30029b2a02b2e1cac1155476d745
Jitendra Singh <52547884+jitendra-singh01@users.noreply.github.com>
2020-11-29 10:53:48 -0500

    Update checkauth.py

--- checkauth.py
+++ checkauth.py
@@ -1,6 +1,8 @@
+#
 from azure.common.client_factory import get_client_from_auth_file
 from azure.mgmt.compute import ComputeManagementClient
 import os
 
 client = get_client_from_auth_file(ComputeManagementClient)
 print(os.environ['AZURE_AUTH_LOCATION']['clientId'])
+
